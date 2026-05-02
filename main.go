39391e4f673608ea72b8b4fa0e3f5b87ec1f1cbc
Jasson López <jasson_alex99@hotmail.com>
2026-05-01 21:13:24 -0600

    Set up GoReleaser release pipeline

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -8,9 +9,24 @@ import (
 	"var-cli/tui"
 )
 
-func main() {
-	m := tui.NewModel()
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
 
+func main() {
+	var showVersion bool
+	flag.BoolVar(&showVersion, "v", false, "show version")
+	flag.BoolVar(&showVersion, "version", false, "show version")
+	flag.Parse()
+
+	if showVersion {
+		fmt.Printf("var-cli %s (commit: %s, built: %s)\n", version, commit, date)
+		os.Exit(0)
+	}
+
+	m := tui.NewModel()
 	p := tea.NewProgram(m)
 
 	if _, err := p.Run(); err != nil {
